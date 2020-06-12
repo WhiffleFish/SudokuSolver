801b326d3bc598bb43f0edbfaa456873e47d559a
WhiffleFish <tylertronics@gmail.com>
2020-06-12 19:02:46 -0400

    Create Model from augmented training data

--- ideas.md
+++ ideas.md
@@ -1,10 +1,12 @@
 # TODO
 - Fill out requirements
 - Operate in venv
-- pickle and export KNN model
+- Isolate Board
+- Augment mnist picture data to account for bars from cell frame (DONE)
 
 # IDEAS
 - Determine cell edges through canny edge detection
 ## For actual pictures of Sudoku puzzle
 - Draw bounding lines around all cells
     - Linear Transform on image so that bouding lines form square
+    - https://www.geeksforgeeks.org/line-detection-python-opencv-houghline-method/
--- trainModel.py
+++ trainModel.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 from tensorflow import keras
 
 # Load Mnist Data
+print('Loading Data...')
 X_train,y_train,X_test,y_test = (mnist.train_images(), 
                                  mnist.train_labels(), 
                                  mnist.test_images(), 
@@ -13,8 +14,31 @@ X_train,y_train,X_test,y_test = (mnist.train_images(),
 X_train = (X_train/255)
 X_test = (X_test/255)
 
-X_train = X_train.reshape(*X_train.shape,1)
-X_test = X_test.reshape(*X_test.shape,1)
+# Augment images to account for cell boundaries 
+def augment_img(image):
+    img = image.copy()
+
+    top,right,bot,left = np.random.random(4) > 0.6
+    if top:
+        size = np.random.randint(0,3)
+        img[:size] = min(np.abs(np.random.normal(loc=0.5, scale=0.5)),1)
+    if right:
+        size = np.random.randint(0,3)
+        img[:,28-size:] = min(np.abs(np.random.normal(loc=0.5, scale=0.5)),1)
+    if bot:    
+        size = np.random.randint(0,3)
+        img[28-size:] = min(np.abs(np.random.normal(loc=0.5, scale=0.5)),1)
+    if left:
+        size = np.random.randint(0,3)
+        img[:,:size] = min(np.abs(np.random.normal(loc=0.5, scale=0.5)),1)
+        
+    return img
+
+print('Augmenting Image Data...')
+aug_X_train = np.array([augment_img(X_train[i]) for i in range(X_train.shape[0])])
+
+X_train_input = aug_X_train.reshape(*aug_X_train.shape,1)
+X_test_input = X_test.reshape(*X_test.shape,1)
 
 # One-hot encode labels
 y_train_onehot = tf.one_hot(y_train, depth=10)
@@ -44,17 +68,16 @@ model.add(keras.layers.Dropout(0.5))
 model.add(keras.layers.Dense(10, activation = "softmax"))
 
 
-
 optimizer = keras.optimizers.Adam(learning_rate=3.0E-4)
 model.compile(optimizer = optimizer , loss = "categorical_crossentropy", metrics=["accuracy"])
 
-epochs = 1
+epochs = 2
 batch_size = 86
 
 # TRAIN MODEL
-history = model.fit(X_train,y_train_onehot, 
+history = model.fit(X_train_input, y_train_onehot, 
                     batch_size=batch_size,
                     epochs=epochs, verbose=2, 
-                    validation_data=(X_test,y_test_onehot))
+                    validation_data=(X_test_input,y_test_onehot))
 
-model.save(r'models\DigitRecognizerModel')
+model.save(r'models\AUG_DigitRecognizerModel')
