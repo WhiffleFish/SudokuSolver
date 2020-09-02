2652d5af9d5c8dc4d462d5ef3e0290aeb58e50c1
WhiffleFish <tylertronics@gmail.com>
2020-09-01 18:33:45 -0600

    Refactor solve_board to be recursive

--- readBoard.py
+++ readBoard.py
@@ -7,6 +7,12 @@ from tensorflow import keras
 
 
 def isolate_board(img):
+    """
+    INPUT:
+        img -- Image containing a Sudoku board
+    OUTPUT:
+        img -- Image cropped to contain only the Sudoku board
+    """
     edges = canny(img, sigma=1).astype(np.uint8)
 
     test_angles = np.linspace(-np.pi/2, np.pi/2, 360)  # Sweep -90° to +90°
@@ -36,6 +42,13 @@ def isolate_board(img):
 
 
 def read_board(img, model=None):
+    """
+    INPUTS:
+        img -- Isolated image of sudoku board
+        model -- Saved Keras CNN model to recognize digits
+    OUTPUT:
+        data -- numpy array of predicted Sudoku board
+    """
     if not model:  # If model isn't specified beforehand
         model = keras.models.load_model(r"models\AUG_DigitRecognizerModel")
 
@@ -70,65 +83,42 @@ def read_board(img, model=None):
     return data
 
 
-def check_row(board, pos):
-    num = board[pos[0]][pos[1]]
-    row = board[pos[0]].copy()
-    row.pop(pos[1])
-    return num not in row
+def isValid(puzzle, c):
+    num = puzzle[c]
+
+    puz = puzzle.copy()
+    puz[c] = 0
+    block = puz[(c[0]//3)*3 : ((c[0]+3)//3)*3, (c[1]//3)*3 : ((c[1]+3)//3)*3]
+
+    # Check Row
+    if num in puz[c[0]]:
+        return False
+    # Check Col
+    elif num in puz[:,c[1]]:
+        return False
+    # Check Block
+    elif num in block:
+        return False
+    else:
+        return True
 
 
-def check_col(board, pos, debug=False):
-    num = board[pos[0]][pos[1]]
-    col = [row[pos[1]] for row in board.copy()]
-    col.pop(pos[0])
-    return num not in col
+def solve_board(board):
+    if 0 not in board:
+        return board
 
+    for i in range(9):
+        for j in range(9):
 
-def check_block(board, pos, debug=False):
-    # Get current Number
-    num = board[pos[0]][pos[1]]
-    # Get upper left block corner
-    block_start = ((int(pos[0]/3)*3), (int(pos[1]/3))*3)
-    # Get lower right block corner
-    block_end = (block_start[0]+3, block_start[1]+3)
+            if board[i, j] == 0:
+                for n in range(1, 10):
+                    board[i, j] = n
 
-    block = [row[block_start[1]:block_end[1]] for row in board[block_start[0]:block_end[0]].copy()]
-    flat_block = [num for row in block for num in row]
-    rel_coord = (pos[0]-block_start[0], pos[1]-block_start[1])
-    rel_index = rel_coord[0]*3 + rel_coord[1]
-    assert flat_block[rel_index] == num
-    flat_block.pop(rel_index)
+                    if isValid(board, (i, j)):
+                        solution = solve_board(board)
+                        if solution is not None:
+                            return solution
 
-    return num not in flat_block
-
-
-def check_num(board, pos):
-    return check_row(board, pos) and check_col(board, pos) and check_block(board, pos)
-
-
-def solve_board(board, c=(0, 0)):
-    # Find next zero
-    current_pos = c[0]*9 + c[1]
-    flattened = [num for row in board for num in row]
-
-    while True:
-        if flattened[current_pos] == 0:
-            break
-        current_pos += 1
-        if current_pos == 9*9:  # No More Zeros
-            return board
-
-    current_coord = (int(current_pos/9), current_pos % 9)
-
-    while True:
-        board[current_coord[0]][current_coord[1]] += 1
-
-        if check_num(board, current_coord):
-            resulting_board = solve_board(board, current_coord)
-            flattened_result_board = [num for row in resulting_board for num in row]
-            if not (0 in flattened_result_board):
-                return resulting_board
-        if board[current_coord[0]][current_coord[1]] >= 9:
-            board[current_coord[0]][current_coord[1]] = 0
-
-            return board
+                # BACHTRACK
+                board[i, j] = 0
+                return None
